311ef8f5ee64887f98a0d91617b07dd696f17ec7
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2017-11-03 02:30:22 +0700

    Update image

--- texture-packer/docker-entrypoint.sh
+++ texture-packer/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-TexturePacker --algorithm Basic --png-opt-level 0 --disable-auto-alias --extrude 0 --trim-mode None --format json --data
+TexturePacker --algorithm Basic --png-opt-level 0 --disable-auto-alias --extrude 0 --trim-mode None --format json --data $@
