df5132be6faa73273ea0fbb559e3187eda9bb681
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2017-11-02 17:30:07 +0700

    Update images

--- dronedocker/docker-exec-swarm.sh
+++ dronedocker/docker-exec-swarm.sh
@@ -5,7 +5,7 @@ DOCKER_SERVICE="${DOCKER_STACK}_$2"
 COMMAND=$3
 
 while : ; do \
-  COUNT_DOWN_SERVICES=$(docker stack services --format='{{.Replicas}}' cigaramobile | grep '0/' | wc -l); \
+  COUNT_DOWN_SERVICES=$(docker stack services --format='{{.Replicas}}' $DOCKER_STACK | grep '0/' | wc -l); \
   COUNT_TASKS=$(docker service ps $DOCKER_SERVICE -q -f desired-state=running -f "name=$DOCKER_SERVICE.1" | wc -l); \
   DOCKER_TASK=$(docker service ps $DOCKER_SERVICE -q -f desired-state=running -f "name=$DOCKER_SERVICE.1"); \
   [ "$COUNT_DOWN_SERVICES" -ne "0" ] || [ "$COUNT_TASKS" -ne "1" ] || break; \
